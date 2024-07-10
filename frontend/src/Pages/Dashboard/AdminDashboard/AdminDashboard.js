7c928b109b7d30e8663a1ccbe5138a52854bebb2
blackadderIII <skobarnes1@gmail.com>
2024-07-10 20:33:09 +0000

    .

--- frontend/src/Pages/Dashboard/AdminDashboard/AdminDashboard.js
+++ frontend/src/Pages/Dashboard/AdminDashboard/AdminDashboard.js
@@ -30,6 +30,25 @@ function AdminDashboard() {
     const [active, setActive] = useState("addbooks")
     const [sidebar, setSidebar] = useState(false)
 
+    const API_URL = process.env.REACT_APP_API_URL;
+     const { user } = useContext(AuthContext);
+    const [memberDetails, setMemberDetails] = useState(null);
+
+    useEffect(() => {
+    const getMemberDetails = async () => {
+      try {
+        const response = await axios.get(
+          API_URL + "api/users/getuser/" + user._id
+        );
+        setMemberDetails(response.data);
+      } catch (err) {
+        console.log("Error in fetching the member details");
+      }
+    };
+    getMemberDetails();
+  }, [API_URL, user]);
+
+
     /* Logout Function*/
     const logout = () => {
         localStorage.removeItem("user");
@@ -62,6 +81,103 @@ function AdminDashboard() {
 
                 </div>
                 <div className="dashboard-option-content">
+
+                <div className="member-profile-content" id="profile@member">
+            <div className="user-details-topbar">
+              <img
+                className="user-profileimage"
+                src="./assets/images/Profile.png"
+                alt=""
+              ></img>
+              <div className="user-info">
+                <p className="user-name">{memberDetails?.userFullName}</p>
+                <p className="user-id">
+                  {memberDetails?.userType === "Student"
+                    ? memberDetails?.admissionId
+                    : memberDetails?.employeeId}
+                </p>
+                <p className="user-email">{memberDetails?.email}</p>
+                <p className="user-phone">{memberDetails?.mobileNumber}</p>
+              </div>
+            </div>
+            <div className="user-details-specific">
+              <div className="specific-left">
+                <div className="specific-left-top">
+                  <p className="specific-left-topic">
+                    <span style={{ fontSize: "18px" }}>
+                      <b>Age</b>
+                    </span>
+                    <span style={{ fontSize: "16px" }}>
+                      {memberDetails?.age}
+                    </span>
+                  </p>
+                  <p className="specific-left-topic">
+                    <span style={{ fontSize: "18px" }}>
+                      <b>Gender</b>
+                    </span>
+                    <span style={{ fontSize: "16px" }}>
+                      {memberDetails?.gender}
+                    </span>
+                  </p>
+                </div>
+                <div className="specific-left-bottom">
+                  <p className="specific-left-topic">
+                    <span style={{ fontSize: "18px" }}>
+                      <b>DOB</b>
+                    </span>
+                    <span style={{ fontSize: "16px" }}>
+                      {memberDetails?.dob}
+                    </span>
+                  </p>
+                  <p className="specific-left-topic">
+                    <span style={{ fontSize: "18px" }}>
+                      <b>Address</b>
+                    </span>
+                    <span style={{ fontSize: "16px" }}>
+                      {memberDetails?.address}
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <div className="specific-right">
+                <div className="specific-right-top">
+                  <p className="specific-right-topic">
+                    <b>Points</b>
+                  </p>
+                  <p
+                    style={{
+                      fontSize: "25px",
+                      fontWeight: "500",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      marginTop: "15px",
+                    }}
+                  >
+                    540
+                  </p>
+                </div>
+                <div className="dashboard-title-line"></div>
+                <div className="specific-right-bottom">
+                  <p className="specific-right-topic">
+                    <b>Rank</b>
+                  </p>
+                  <p
+                    style={{
+                      fontSize: "25px",
+                      fontWeight: "500",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      marginTop: "15px",
+                    }}
+                  >
+                    {memberDetails?.points}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
                     <div className="dashboard-addbooks-content" style={active !== "addbook" ? { display: 'none' } : {}}>
                         <AddBook />
                     </div>
