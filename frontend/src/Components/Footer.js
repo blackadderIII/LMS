496f02eb58cbad5f68b8b9cc6840bf7a68d04b49
blackadderIII <skobarnes1@gmail.com>
2024-07-11 12:00:39 +0000

    .

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -27,9 +27,9 @@ function Footer() {
                     </div>
                 </div>
                 <div className="contact-social" >
-                    <a href='https://x.com/knustgh?s=21&t=iTGxFnGwbXuhh-I9zocTGw' target='_blank'  className='social-icon'><TwitterIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
-                    <a href='https://www.linkedin.com/groups/772157' target='_blank' className='social-icon'><LinkedInIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
-                    <a href='https://www.instagram.com/knust.gh?igsh=MW5zb3hsdDE2aTZhcw==' target='_blank' className='social-icon'><InstagramIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                    <a href='https://x.com/knustgh?s=21&t=iTGxFnGwbXuhh-I9zocTGw' target='_blank' rel="noreferrer"   className='social-icon'><TwitterIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                    <a href='https://www.linkedin.com/groups/772157' target='_blank'rel="noreferrer"  className='social-icon'><LinkedInIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
+                    <a href='https://www.instagram.com/knust.gh?igsh=MW5zb3hsdDE2aTZhcw==' target='_blank' rel="noreferrer" className='social-icon'><InstagramIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
                 </div>
             </div>
             <div className='copyright-details'>
