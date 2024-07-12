a8b94c5b451fd5757560865e3a1531e2c1d6e692
blackadderIII <skobarnes1@gmail.com>
2024-07-12 10:35:54 +0000

    .

--- frontend/src/Components/RecentAddedBooks.js
+++ frontend/src/Components/RecentAddedBooks.js
@@ -1,32 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './RecentAddedBooks.css'
+import axios from 'axios'
 
 function RecentAddedBooks() {
+    const API_URL = process.env.REACT_APP_API_URL
+    const [recentBook,setrecentBook] = useState([])
+    useEffect(()=>{
+            const getRecentBooks = async ()=>{
+                try{
+                    const response = await axios.get(API_URL+'api/books/allbooks');
+                    setrecentBook(response.data)
+                    return
+                }
+                catch(error){
+                    console.log(error)
+                }
+            }
+            getRecentBooks()
+    },[API_URL])
+
     return (
         <div className='recentaddedbooks-container'>
             <h className='recentbooks-title'>Recent Uploads</h>
             <div className='recentbooks'>
                 <div className='images'>
-                    <img className='recent-book' src='https://inkinmytea.files.wordpress.com/2011/12/apj.jpg?w=640' alt=''></img>
-                    <img className='recent-book' src='https://images-na.ssl-images-amazon.com/images/I/91VokXkn8hL.jpg' alt=''></img>
-                    <img className='recent-book' src='https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg' alt=''></img>
-                    <img className='recent-book' src='https://images-na.ssl-images-amazon.com/images/I/71m-MxdJ2WL.jpg' alt=''></img>
-                    <img className='recent-book' src='https://images-na.ssl-images-amazon.com/images/I/71t4GuxLCuL.jpg' alt=''></img>
-                    <img className='recent-book' src='https://19en282jw7pc3zpwj22pg8v0-wpengine.netdna-ssl.com/wp-content/uploads/2021/01/Good-to-Great-Jim-Collins.jpg' alt=''></img>
-                    <img className='recent-book' src='https://images-na.ssl-images-amazon.com/images/I/81mXQdi5x+L.jpg' alt=''></img>
-                    <img className='recent-book' src='https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1498813353l/34267304.jpg' alt=''></img>
-                    <img className='recent-book' src='https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1588152105' alt=''></img>
+                {recentBook.map((book)=> <img className='recent-book' key={book._id} src={book.bookCoverImage ? `data:image/jpeg;base64,${book.bookCoverImage}` : "assets/coverImages/default.png"} alt=''></img> )}
                 </div>
                 <div className='images'>
-                    <img className='recent-book' src='https://inkinmytea.files.wordpress.com/2011/12/apj.jpg?w=640' alt=''></img>
-                    <img className='recent-book' src='https://images-na.ssl-images-amazon.com/images/I/91VokXkn8hL.jpg' alt=''></img>
-                    <img className='recent-book' src='https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg' alt=''></img>
-                    <img className='recent-book' src='https://images-na.ssl-images-amazon.com/images/I/71m-MxdJ2WL.jpg' alt=''></img>
-                    <img className='recent-book' src='https://images-na.ssl-images-amazon.com/images/I/71t4GuxLCuL.jpg' alt=''></img>
-                    <img className='recent-book' src='https://19en282jw7pc3zpwj22pg8v0-wpengine.netdna-ssl.com/wp-content/uploads/2021/01/Good-to-Great-Jim-Collins.jpg' alt=''></img>
-                    <img className='recent-book' src='https://images-na.ssl-images-amazon.com/images/I/81mXQdi5x+L.jpg' alt=''></img>
-                    <img className='recent-book' src='https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1498813353l/34267304.jpg' alt=''></img>
-                    <img className='recent-book' src='https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1588152105' alt=''></img>
+                {recentBook.map((book)=> <img className='recent-book' key={book._id} src={book.bookCoverImage ? `data:image/jpeg;base64,${book.bookCoverImage}` : "assets/coverImages/default.png"} alt=''></img> )}
                 </div>
             </div>
         </div>
