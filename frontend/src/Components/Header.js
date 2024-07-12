5677f53fdad2f50b13c7ebea08acfd8ede9b4b09
blackadderIII <skobarnes1@gmail.com>
2024-07-12 12:41:36 +0000

    .

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,59 +1,87 @@
-import { React, useState } from 'react'
-import { Link } from 'react-router-dom'
-import './Header.css'
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import "./Header.css";
 
-import MenuIcon from '@material-ui/icons/Menu';
-import ClearIcon from '@material-ui/icons/Clear';
+import MenuIcon from "@material-ui/icons/Menu";
+import ClearIcon from "@material-ui/icons/Clear";
 
 function Header() {
+  const [menutoggle, setMenutoggle] = useState(false);
+  const location = useLocation();
 
-    const [menutoggle, setMenutoggle] = useState(false)
+  const Toggle = () => {
+    setMenutoggle(!menutoggle);
+  };
 
-    const Toggle = () => {
-        setMenutoggle(!menutoggle)
-    }
+  const closeMenu = () => {
+    setMenutoggle(false);
+  };
 
-    const closeMenu = () => {
-        setMenutoggle(false)
-    }
-
-    return (
-        <div className="header">
-            <div className="logo-nav">
-            <Link to='/'>
-                <a href="#home">KLS</a>
+  return (
+    <div className="header">
+      <div className="logo-nav">
+        <Link to="/">
+          <a href="#home">KLS</a>
+        </Link>
+      </div>
+      <div className="nav-right">
+        {location.pathname === "/books" && (
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search a Book"
+            style={{ visibility: "visible" }}
+          />
+        )}
+        <ul className={menutoggle ? "nav-options active" : "nav-options"}>
+          <li
+            className="option"
+            onClick={() => {
+              closeMenu();
+            }}
+          >
+            <Link to="/">
+              <a href="#home">Home</a>
             </Link>
-            </div>
-            <div className='nav-right'>
-                <input className='search-input' type='text' placeholder='Search a Book'/>
-                <ul className={menutoggle ? "nav-options active" : "nav-options"}>
-                    <li className="option" onClick={() => { closeMenu() }}>
-                        <Link to='/'>
-                            <a href="#home">Home</a>
-                        </Link>
-                    </li>
-                    <li className="option" onClick={() => { closeMenu() }}>
-                        <Link to='/books'>
-                        <a href="#books">Books</a>
-                        </Link>
-                    </li>
-                    <li className="option" onClick={() => { closeMenu() }}>
-                        <Link to='/signin'>
-                        <a href='signin'>SignIn</a>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
+          </li>
+          <li
+            className="option"
+            onClick={() => {
+              closeMenu();
+            }}
+          >
+            <Link to="/books">
+              <a href="#books">Books</a>
+            </Link>
+          </li>
+          <li
+            className="option"
+            onClick={() => {
+              closeMenu();
+            }}
+          >
+            <Link to="/signin">
+              <a href="signin">SignIn</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
 
-            <div className="mobile-menu" onClick={() => { Toggle() }}>
-                {menutoggle ? (
-                    <ClearIcon className="menu-icon" style={{ fontSize: 40 }} />
-                ) : (
-                    <MenuIcon className="menu-icon" style={{ fontSize: 40 }} />
-                )}
-            </div>
-        </div>
-    )
+      <div
+        className="mobile-menu"
+        onClick={() => {
+          Toggle();
+        }}
+      >
+        {menutoggle ? (
+          <ClearIcon className="menu-icon" style={{ fontSize: 40 }} />
+        ) : (
+          <MenuIcon className="menu-icon" style={{ fontSize: 40 }} />
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
