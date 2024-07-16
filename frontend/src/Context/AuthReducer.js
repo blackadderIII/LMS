b18da1601c34f544d64b07fb00cad7e55c183ea6
blackadderIII <skobarnes1@gmail.com>
2024-07-16 19:48:14 +0000

    .

--- frontend/src/Context/AuthReducer.js
+++ frontend/src/Context/AuthReducer.js
@@ -1,70 +1,54 @@
-const AuthReducer = (state, action) => {
-  switch (action.type) {
-    case "LOGIN_START":
-      return { ...state, isFetching: true, error: false };
-    case "LOGIN_SUCCESS":
-      return {
-        ...state,
-        isFetching: false,
-        isLoggedIn: true,
-        user: action.payload,
-      };
-    case "LOGIN_FAILURE":
-      return {
-        ...state,
-        isFetching: false,
-        error: true,
-        payload: action.payload,
-      };
-    // case "SIGNUP_START":
-    //   return { ...state, isFetching: true, error: false };
-    // case "SIGNUP_SUCCESS":
-    //   return {
-    //     ...state,
-    //     isFetching: false,
-    //     isLoggedIn: true,
-    //     user: action.payload,
-    //   };
-    // case "SIGNUP_FAILURE":
-    //   return {
-    //     ...state,
-    //     isFetching: false,
-    //     error: true,
-    //     payload: action.payload,
-    //   };
-    case "LOGOUT":
-      return { ...state, isLoggedIn: false, user: null };
-    default:
-      return state;
-  }
-};
-
-export default AuthReducer;
-
 // const AuthReducer = (state, action) => {
-//     switch (action.type) {
-//         case "LOGIN_START":
-//             return {
-//                 user: action.payload,
-//                 isLoading: true,
-//                 error: false
-//             };
-//         case "LOGIN_SUCCESS":
-//             return {
-//                 user: action.payload,
-//                 isLoading: false,
-//                 error: false
-//             };
-//         case "LOGIN_FAILURE":
-//             return {
-//                 user: null,
-//                 isLoading: false,
-//                 error: action.payload
-//             };
-
-//         default:
-//             return state
-//     }
-// }
+//   switch (action.type) {
+//     case "LOGIN_START":
+//       return { ...state, isFetching: true, error: false };
+//     case "LOGIN_SUCCESS":
+//       return {
+//         ...state,
+//         isFetching: false,
+//         isLoggedIn: true,
+//         user: action.payload,
+//       };
+//     case "LOGIN_FAILURE":
+//       return {
+//         ...state,
+//         isFetching: false,
+//         error: true,
+//         payload: action.payload,
+//       };
+//     case "LOGOUT":
+//       return { ...state, isLoggedIn: false, user: null };
+//     default:
+//       return state;
+//   }
+// };
 
 // export default AuthReducer;
+
+const AuthReducer = (state, action) => {
+    switch (action.type) {
+        case "LOGIN_START":
+            return {
+                user: action.payload,
+                isLoading: true,
+                error: false
+            };
+        case "LOGIN_SUCCESS":
+            return {
+                user: action.payload,
+                isLoading: false,
+                error: false
+            };
+        case "LOGIN_FAILURE":
+            return {
+                user: null,
+                isLoading: false,
+                error: action.payload
+            };
+
+        default:
+            return state
+    }
+}
+
+export default AuthReducer;
