0e6030fcdec1467e6185a2e7876b9a6bf9ce7a35
blackadderIII <skobarnes1@gmail.com>
2024-07-17 14:55:52 +0000

    .

--- frontend/src/Pages/Dashboard/MemberDashboard/MemberDashboard.js
+++ frontend/src/Pages/Dashboard/MemberDashboard/MemberDashboard.js
@@ -61,7 +61,7 @@ function MemberDashboard() {
         >
           <div className="dashboard-logo">
             <LibraryBooksIcon style={{ fontSize: 50 }} />
-            <p className="logo-name">LCMS</p>
+            <p className="logo-name">KLS</p>
           </div>
           <a
             href="#profile@member"
