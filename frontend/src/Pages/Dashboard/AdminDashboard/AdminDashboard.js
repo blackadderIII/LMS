516beb171899e00fd3c10eca3af42735d00a6085
blackadderIII <skobarnes1@gmail.com>
2024-07-22 10:39:36 +0000

    .

--- frontend/src/Pages/Dashboard/AdminDashboard/AdminDashboard.js
+++ frontend/src/Pages/Dashboard/AdminDashboard/AdminDashboard.js
@@ -1,44 +1,43 @@
 import React, { useContext, useEffect, useState } from "react";
-import "./AdminDashboard.css"
-import AddTransaction from './Components/AddTransaction'
-import AddMember from './Components/AddMember'
-import AddBook from './Components/AddBook';
+import "./AdminDashboard.css";
+import AddTransaction from "./Components/AddTransaction";
+import AddMember from "./Components/AddMember";
+import AddBook from "./Components/AddBook";
 
 import { AuthContext } from "../../../Context/AuthContext";
 import axios from "axios";
 
-import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import BookIcon from '@material-ui/icons/Book';
-import ReceiptIcon from '@material-ui/icons/Receipt';
-import PersonAddIcon from '@material-ui/icons/PersonAdd';
-import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
-import { IconButton } from '@material-ui/core';
-import CloseIcon from '@material-ui/icons/Close';
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import GetMember from './Components/GetMember';
-import AssignmentReturnIcon from '@material-ui/icons/AssignmentReturn';
-import Return from './Components/Return';
-import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
-
-
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import BookIcon from "@material-ui/icons/Book";
+import ReceiptIcon from "@material-ui/icons/Receipt";
+import PersonAddIcon from "@material-ui/icons/PersonAdd";
+import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
+import { IconButton } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import GetMember from "./Components/GetMember";
+import AssignmentReturnIcon from "@material-ui/icons/AssignmentReturn";
+import Return from "./Components/Return";
+import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
 
 /* Semantic UI Dropdown Styles Import */
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
-styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+styleLink.href =
+  "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
 document.head.appendChild(styleLink);
 
 function AdminDashboard() {
+  const [active, setActive] = useState("profile");
+  const [sidebar, setSidebar] = useState(false);
 
-    const [active, setActive] = useState("profile")
-    const [sidebar, setSidebar] = useState(false)
+  const API_URL = process.env.REACT_APP_API_URL;
+  const { user } = useContext(AuthContext);
+  const [memberDetails, setMemberDetails] = useState(null);
+  const [allmembers, setAllMembers] = useState(null);
 
-    const API_URL = process.env.REACT_APP_API_URL;
-     const { user } = useContext(AuthContext);
-    const [memberDetails, setMemberDetails] = useState(null);
-
-    useEffect(() => {
+  useEffect(() => {
     const getMemberDetails = async () => {
       try {
         const response = await axios.get(
@@ -52,41 +51,125 @@ function AdminDashboard() {
     getMemberDetails();
   }, [API_URL, user]);
 
+  //Fetch Members
+  useEffect(() => {
+    const getMembers = async () => {
+      try {
+        const response = await axios.get(API_URL + "api/users/getallmembers");
+        setAllMembers(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMembers();
+  }, [API_URL]);
 
-    /* Logout Function*/
-    const logout = () => {
-        localStorage.removeItem("user");
-        window.location.reload();
-    }
+  /* Logout Function*/
+  const logout = () => {
+    localStorage.removeItem("user");
+    window.location.reload();
+  };
 
+  return (
+    <div className="dashboard">
+      <div className="dashboard-card">
+        <div className="sidebar-toggler" onClick={() => setSidebar(!sidebar)}>
+          <IconButton>
+            {sidebar ? (
+              <CloseIcon style={{ fontSize: 25, color: "rgb(234, 68, 74)" }} />
+            ) : (
+              <DoubleArrowIcon
+                style={{ fontSize: 25, color: "rgb(234, 68, 74)" }}
+              />
+            )}
+          </IconButton>
+        </div>
 
-    return (
-        <div className="dashboard">
-            <div className="dashboard-card">
-                <div className="sidebar-toggler" onClick={() => setSidebar(!sidebar)}>
-                    <IconButton>
-                        {sidebar ? <CloseIcon style={{ fontSize: 25, color: "rgb(234, 68, 74)" }} /> : <DoubleArrowIcon style={{ fontSize: 25, color: "rgb(234, 68, 74)" }} />}
-                    </IconButton>
-                </div>
-
-                {/* Sidebar */}
-                <div className={sidebar ? "dashboard-options active" : "dashboard-options"}>
-                    <div className='dashboard-logo'>
-                        <LibraryBooksIcon style={{ fontSize: 50 }} />
-                        <p className="logo-name">KLS</p>
-                    </div>
-                    <p className={`dashboard-option ${active === "profile" ? "clicked" : ""}`} onClick={() => { setActive("profile"); setSidebar(false) }}><AccountCircleIcon className='dashboard-option-icon' /> Profile</p>
-                    <p className={`dashboard-option ${active === "addbook" ? "clicked" : ""}`} onClick={() => { setActive("addbook"); setSidebar(false) }}><BookIcon className='dashboard-option-icon' />Add Book</p>
-                    <p className={`dashboard-option ${active === "addtransaction" ? "clicked" : ""}`} onClick={() => { setActive("addtransaction"); setSidebar(false) }}><ReceiptIcon className='dashboard-option-icon' /> Add Transaction </p>
-                    <p className={`dashboard-option ${active === "getmember" ? "clicked" : ""}`} onClick={() => { setActive("getmember"); setSidebar(false) }}><AccountBoxIcon className='dashboard-option-icon' /> Get Member </p>
-                    <p className={`dashboard-option ${active === "addmember" ? "clicked" : ""}`} onClick={() => { setActive("addmember"); setSidebar(false) }}><PersonAddIcon className='dashboard-option-icon' /> Add Member </p>
-                    <p className={`dashboard-option ${active === "returntransaction" ? "clicked" : ""}`} onClick={() => { setActive("returntransaction"); setSidebar(false) }}><AssignmentReturnIcon className='dashboard-option-icon' /> Return </p>
-                    <p className={`dashboard-option`} onClick={logout}><PowerSettingsNewIcon className='dashboard-option-icon' /> Log out </p>
-
-                </div>
-                <div className="dashboard-option-content">
-
-                    <div className="member-profile-content" style={active !== "profile" ? { display: 'none' } : {}} id="profile@member">
+        {/* Sidebar */}
+        <div
+          className={sidebar ? "dashboard-options active" : "dashboard-options"}
+        >
+          <div className="dashboard-logo">
+            <LibraryBooksIcon style={{ fontSize: 50 }} />
+            <p className="logo-name">KLS</p>
+          </div>
+          <p
+            className={`dashboard-option ${
+              active === "profile" ? "clicked" : ""
+            }`}
+            onClick={() => {
+              setActive("profile");
+              setSidebar(false);
+            }}
+          >
+            <AccountCircleIcon className="dashboard-option-icon" /> Profile
+          </p>
+          <p
+            className={`dashboard-option ${
+              active === "addbook" ? "clicked" : ""
+            }`}
+            onClick={() => {
+              setActive("addbook");
+              setSidebar(false);
+            }}
+          >
+            <BookIcon className="dashboard-option-icon" />
+            Add Book
+          </p>
+          <p
+            className={`dashboard-option ${
+              active === "addtransaction" ? "clicked" : ""
+            }`}
+            onClick={() => {
+              setActive("addtransaction");
+              setSidebar(false);
+            }}
+          >
+            <ReceiptIcon className="dashboard-option-icon" /> Add Transaction{" "}
+          </p>
+          <p
+            className={`dashboard-option ${
+              active === "getmember" ? "clicked" : ""
+            }`}
+            onClick={() => {
+              setActive("getmember");
+              setSidebar(false);
+            }}
+          >
+            <AccountBoxIcon className="dashboard-option-icon" /> Get Member{" "}
+          </p>
+          <p
+            className={`dashboard-option ${
+              active === "addmember" ? "clicked" : ""
+            }`}
+            onClick={() => {
+              setActive("addmember");
+              setSidebar(false);
+            }}
+          >
+            <PersonAddIcon className="dashboard-option-icon" /> Add Member{" "}
+          </p>
+          <p
+            className={`dashboard-option ${
+              active === "returntransaction" ? "clicked" : ""
+            }`}
+            onClick={() => {
+              setActive("returntransaction");
+              setSidebar(false);
+            }}
+          >
+            <AssignmentReturnIcon className="dashboard-option-icon" /> Return{" "}
+          </p>
+          <p className={`dashboard-option`} onClick={logout}>
+            <PowerSettingsNewIcon className="dashboard-option-icon" /> Log out{" "}
+          </p>
+        </div>
+        <div className="dashboard-option-content">
+          <div
+            className="member-profile-content"
+            style={active !== "profile" ? { display: "none" } : {}}
+            id="profile@member"
+          >
             <div className="user-details-topbar">
               <img
                 className="user-profileimage"
@@ -143,65 +226,82 @@ function AdminDashboard() {
                   </p>
                 </div>
               </div>
-              {/* <div className="specific-right">
-                <div className="specific-right-top">
-                  <p className="specific-right-topic">
-                    <b>Points</b>
-                  </p>
-                  <p
-                    style={{
-                      fontSize: "25px",
-                      fontWeight: "500",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      marginTop: "15px",
-                    }}
-                  >
-                    540
-                  </p>
-                </div>
-                <div className="dashboard-title-line"></div>
-                <div className="specific-right-bottom">
-                  <p className="specific-right-topic">
-                    <b>Rank</b>
-                  </p>
-                  <p
-                    style={{
-                      fontSize: "25px",
-                      fontWeight: "500",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      marginTop: "15px",
-                    }}
-                  >
-                    {memberDetails?.points}
-                  </p>
-                </div>
-              </div> */}
             </div>
-                    </div>
-                    
-                    <div className="dashboard-addbooks-content" style={active !== "addbook" ? { display: 'none' } : {}}>
-                        <AddBook />
-                    </div>
-                    <div className="dashboard-transactions-content" style={active !== "addtransaction" ? { display: 'none' } : {}}>
-                        <AddTransaction />
-                    </div>
-                    <div className="dashboard-addmember-content" style={active !== "addmember" ? { display: 'none' } : {}}>
-                        <AddMember />
-                    </div>
-                    <div className="dashboard-addmember-content" style={active !== "getmember" ? { display: 'none' } : {}}>
-                        <GetMember />
-                    </div>
-                    <div className="dashboard-addmember-content" style={active !== "returntransaction" ? { display: 'none' } : {}}>
-                        <Return />
-                    </div>
-                </div>
+            <div className="user-details-leaderboard">
+            <h3 style={{color:"green",marginLeft:"30%",marginTop:"2.5%"}}>KNUST Library System Top Members</h3>
+              <table className="member-leaderboard">
+                <tr>
+                  <th>S.No</th>
+                  <th>User</th>
+                  <th>ID</th>
+                  <th>User Type</th>
+                  <th>Number</th>
+                  <th>Email</th>
+                  <th>Points</th>
+                </tr>
+                {allmembers !== null && allmembers.length > 0 ? (
+                  allmembers
+                    .sort((a, b) => b.points - a.points) // sort by points in descending order
+                    .map((user, index) => (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{user.userFullName}</td>
+                        <td>
+                          {user.admissionId.trim() === ""
+                            ? user.employeeId
+                            : user.admissionId}
+                        </td>
+                        <td>{user.userType}</td>
+                        <td>{user.mobileNumber}</td>
+                        <td>
+                          <a href={`mailto:${user.email}`}>{user.email}</a>
+                        </td>
+                        <td>{user.points}</td>
+                      </tr>
+                    ))
+                ) : (
+                  <tr>
+                    <td colSpan={7}>No members found</td>
+                  </tr>
+                )}
+              </table>
             </div>
+          </div>
+
+          <div
+            className="dashboard-addbooks-content"
+            style={active !== "addbook" ? { display: "none" } : {}}
+          >
+            <AddBook />
+          </div>
+          <div
+            className="dashboard-transactions-content"
+            style={active !== "addtransaction" ? { display: "none" } : {}}
+          >
+            <AddTransaction />
+          </div>
+          <div
+            className="dashboard-addmember-content"
+            style={active !== "addmember" ? { display: "none" } : {}}
+          >
+            <AddMember />
+          </div>
+          <div
+            className="dashboard-addmember-content"
+            style={active !== "getmember" ? { display: "none" } : {}}
+          >
+            <GetMember />
+          </div>
+          <div
+            className="dashboard-addmember-content"
+            style={active !== "returntransaction" ? { display: "none" } : {}}
+          >
+            <Return />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default AdminDashboard
+export default AdminDashboard;
